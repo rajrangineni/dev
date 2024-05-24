f548f9aa807e2a234e16fbc9faa95fae50d3c02b
rajrangineni <r6301797747@gmail.com>
2024-05-24 17:08:25 +0530

    jjjh

--- resource.tf
+++ resource.tf
@@ -1,4 +1,13 @@
-resource "aws_instance" "import" {
-  ami = "ami-0cc9838aa7ab1dce7"
-  instance_type = "t2.micro"
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "test" {
+    ami = "ami-0cc9838aa7ab1dce7"
+    instance_type = "t2.micro"
+    # subnet_id = "subnet-0a509e41d91a981ec"
+    key_name = "mykey"
+    
+  tags = {
+    Name = "test"
+  }
 }
