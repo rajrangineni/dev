b3e51ca3bf60643260c06cdd97d6f6148ef679b4
rajrangineni <168529818+rajrangineni@users.noreply.github.com>
2024-05-27 17:47:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,24 @@
 node {
     def mvnHome
     stage('clone') { 
-        git branch: 'main', url: 'https://github.com/rajrangineni/dev.git'
+        git branch: 'main', url: 'https://github.com/rajrangineni/nikki.git'
+        dir('day01_terraform'){
+           }
     }
     stage('init') {
+dir('day01_terraform'){
         sh 'terraform init'
+            }
     }
     stage('plan') {
+       dir('day01_terraform'){
        sh 'terraform plan'
+            }
     }
      stage('apply') {
+         dir('day01_terraform'){
        sh 'terraform apply -auto-approve'
+            }
+       
     }
 }
