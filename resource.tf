b475b0fa6ad837ff56bd8366e98e0d5ae180252e
rajrangineni <168529818+rajrangineni@users.noreply.github.com>
2024-05-27 15:50:16 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,13 +1,14 @@
 provider "aws" {
-  region = "ap-south-1"
+    
+  
 }
-resource "aws_instance" "test" {
+resource "aws_instance" "name" {
     ami = "ami-0cc9838aa7ab1dce7"
     instance_type = "t2.micro"
-    # subnet_id = "subnet-0a509e41d91a981ec"
     key_name = "mykey"
-    
-  tags = {
-    Name = "test"
-  }
+    tags = {
+      Name = "test"
+
+    }
+  
 }
