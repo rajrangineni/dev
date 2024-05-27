6ad8a253ffb7d343248e2bbcc089be62c2675da7
rajrangineni <168529818+rajrangineni@users.noreply.github.com>
2024-05-27 17:36:12 +0530

    Create resource.tf

---
+++ resource.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    
+  
+}
+resource "aws_instance" "name" {
+    ami = "ami-0cc9838aa7ab1dce7"
+    instance_type = "t2.micro"
+    key_name = "mykey"
+    tags = {
+      Name = "test"
+
+    }
+  
+}
