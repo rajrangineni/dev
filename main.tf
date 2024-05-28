8e8b0f610e1de0150c6fbc8d0d6ec2b7456de90c
rajrangineni <168529818+rajrangineni@users.noreply.github.com>
2024-05-28 15:59:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,23 +2,22 @@ node {
     def mvnHome
     stage('clone') { 
     git branch: 'main', url: 'https://github.com/rajrangineni/dev.git'
-        dir('resource.tf'){
-           }
+      
     }
     stage('init') {
-dir('resource.tf'){
+
         sh 'terraform init'
-            }
+            
     }
     stage('plan') {
-       dir('resource.tf'){
+      
        sh 'terraform plan'
-            }
+            
     }
      stage('apply') {
-         dir('resource.tf'){
+        
        sh 'terraform apply -auto-approve'
             }
        
-    }
+    
 }
