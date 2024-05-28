2d5e806eca9d7ba2cdc28db51c62d1486f20cb4b
rajrangineni <168529818+rajrangineni@users.noreply.github.com>
2024-05-28 16:24:43 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "raj" {
-    ami = ami-0cc9838aa7ab1dce7
-    instance_type = t2.micro
-    key_name = mykey
+    ami = "ami-0cc9838aa7ab1dce7"
+    instance_type = "t2.micro"
+    key_name = "mykey"
     tags = {
       Name = "raj"
 
